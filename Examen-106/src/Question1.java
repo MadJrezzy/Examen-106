9ef54e7fd98c435d417c01652448c598e6d3173c
a0909283 <a0909283@TP-401-0037333.info.csaffluents.qc.ca>
2023-11-13 16:31:38 -0500

    exam

---
+++ Examen-106/src/Question1.java
@@ -0,0 +1,21 @@
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		
+		int [] tabSource  = new int [10], tabDestination = new int[10];
+
+		
+		int nbRandom = (int) (Math.random() * 100) + 0; 
+		
+		for (int i = 0; i < tabSource.length; i++) {
+			
+			if ( nbRandom[i] % 3) {
+				
+				
+				
+			}
+		}
+	}
+
+}
